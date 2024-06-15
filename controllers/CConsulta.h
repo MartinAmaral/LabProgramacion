d6ae95094b8c50048f80fa0bfe1135921962e2f2
PatricioSuarez13 <patriciorsuarez@hotmail.com>
2024-06-15 19:41:54 -0300

    comiteo nomas el CConsulta.h porque hay baja intensidad en casa ajhsajsaj

--- controllers/CConsulta.h
+++ controllers/CConsulta.h
@@ -1,2 +1,19 @@
 #include <string>
 #include "../entidades/usuario.h"
+#include "../entidades/medico.h"
+#include "../entidades/fecha.h"
+// Este controllador se encarga de los siguientes casos de uso:
+// Registro de Consulta
+// Alta de Diagnostico de Una Consulta
+
+class CDiagnostico {
+    private:
+        
+    public:
+        //Ingresa los datos de consulta
+        void ingresarDatosConsulta(string ciMedico, string ciPaciente);
+        //Ingresa los datos de una consulta de emergencia
+        void ingresarDatosConsultaEmergencia();
+        //Ingresa los datos de una consulta comun
+        void ingresarDatosConsultaComun();
+};
