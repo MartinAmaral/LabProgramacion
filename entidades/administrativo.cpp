614e515895686b981c9e294894059aff242002ce
PatricioSuarez13 <patriciorsuarez@hotmail.com>
2024-06-18 21:06:25 -0300

    se agrega logica de administrativo.cpp, feliz cumple ema lol

--- entidades/administrativo.cpp
+++ entidades/administrativo.cpp
@@ -1,4 +1,26 @@
 #include "administrativo.h"
 #include <iostream>
 
+// Constructor de Administrativo
+Administrativo::Administrativo(bool activo, int CI, Sexo sexo, Fecha* fechaNacimiento, string nombre, string apellido, string contrasena)
+    : Usuario(nombre, apellido, sexo, CI, fechaNacimiento) {
+    this->setActivo(activo);
+    this->setContrasena(contrasena);
+}
 
+// Funciones heredadas de Usuario
+int Administrativo::getCI() {
+    return Usuario::getCI();
+}
+
+bool Administrativo::comprobarPass(string pass) {
+    return Usuario::comprobarPass(pass);
+}
+
+string Administrativo::getContrasena() {
+    return Usuario::getContrasena();
+}
+
+int Administrativo::getEdad() {
+    return Usuario::getEdad();
+}
